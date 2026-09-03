ed4be9755ca34822bd6e52f4d97948e17be54d13
Upendra Kushwaha <166939048+ukiaf11@users.noreply.github.com>
2026-09-03 16:22:51 +0530

    Delete my-blog directory

--- my-blog/app/api/posts/route.ts
+++
@@ -1,7 +0,0 @@
-import { getSortedPostsData } from "@/lib/posts";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    const posts = getSortedPostsData();
-    return NextResponse.json(posts);
-}
